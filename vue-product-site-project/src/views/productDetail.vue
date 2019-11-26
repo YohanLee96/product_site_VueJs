906b948551b45da24d72d91d583ca711c9d9075d
Yohan Lee <swj03095@naver.com>
2019-11-26 18:31:18 +0900

    수정

--- vue-product-site-project/src/views/productDetail.vue
+++ vue-product-site-project/src/views/productDetail.vue
@@ -28,11 +28,7 @@
                 <aside class="col-sm-4">
                     <article class="gallery-wrap"> 
                         <div class="img-big-wrap">
-                            <div>
-                                <a href="#">
-                                    <img v-bind:src="path">
-                                </a>
-                            </div>
+                            <img class="productImg" v-bind:src="path">
                         </div>
                         <div class="img-small-wrap">
                         <!-- <div class="item-gallery"> <img src="https://via.placeholder.com/100x100"> </div>
@@ -42,27 +38,76 @@
                         </div>
                     </article>
                 </aside>
-                <aside class="col-sm-3">
-                    <div class="row mt-5">                
-                        <span>색상/사이즈</span> &nbsp;
-                        <select class="form-control form-control-sm" style="width:50px;">
-                            <option value="">1</option>
-                            <option value="">2</option>
-                            <option value="">3</option>
+                <aside class="infoClass">
+                    <div class="row">                
+                        <span>색상/모델 : </span>
+                        <select class="form-control">
+                            <option value="블랙-1.6Gdi">블랙-1.6Gdi</option>
+                            <option value="블랙-2.0Gdi">블랙-2.0Gdi</option>
+                            <option value="레드-1.6Gdi">레드-1.6Gdi</option>
+                            <option value="레드-2.0Gdi">레드-2.0Gdi</option>
                         </select>
                     </div>
                     <div class="row">
-                        <button class="btn btn-lg color-box-waanbii" type="button"> <i class="fa fa-shopping-cart"></i> Ajouter au panier </button>
+                        <table class="table" style="border:">
+                            <tbody>
+                                <tr>
+                                    <th scope="row">
+                                        블랙-1.6Gdi
+                                    </th>
+                                    <td>
+                                        <button type="button" class="btn btn-light">-</button>
+                                        <input type="text" value="1"/>
+                                        <button type="button" class="btn btn-light">+</button>
+                                    </td>
+                                    <td>
+                                        20,000원
+                                    </td>
+                                    <td>
+                                        <button type="button" class="btn btn-light">X</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        블랙-2.0Gdi
+                                    </th>
+                                    <td>
+                                        <button type="button" class="btn btn-light">-</button>
+                                        <input type="text" value="1"/>
+                                        <button type="button" class="btn btn-light">+</button>
+                                    </td>
+                                    <td>
+                                        20,000원
+                                    </td>
+                                    <td>
+                                        <button type="button" class="btn btn-light">X</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        레드-1.6Gdi
+                                    </th>
+                                    <td>
+                                        <button type="button" class="btn btn-light">-</button>
+                                        <input type="text" value="1"/>
+                                        <button type="button" class="btn btn-light">+</button>
+                                    </td>
+                                    <td>
+                                        20,000원
+                                    </td>
+                                    <td>
+                                        <button type="button" class="btn btn-light">X</button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
-                    <div class="row mb-4 mt-4">
-                        <button class="btn btn-lg color-box-waanbii" type="button"> <i class="fa fa-credit-card"></i> Passer la commande </button>
+                    <div class="row">
+                        <h3 style="text-align:right">60,000원(3개)</h3>
                     </div>
-                    <div class="row mb-4 mt-4">
-                        <button class="btn btn-lg btn-success" type="button"><i class="fa fa-heart fa-beat"></i></button>
-                    </div>
-                    <hr class="m-0 p-0">
-                    <div class="row mb-4 mt-4">
-                        Il reste 3 exemplaires du produits.
+                    <div class="row">
+                       <button type="button" class="btn btn-primary btn-lg btn-block">장바구니</button>
+                       <button type="button" class="btn btn-secondary btn-lg btn-block">바로 구매하기</button>
                     </div>
                 </aside>
             </div>
@@ -89,13 +134,20 @@ export default {
     methods : {
          getJson : function(){  //json 데이터 가져옴.
              let vm = this;
+             if(vm.$route.params.name == undefined){    //Routing이 아닌 새로고침 또는 URL로 직접쳐서들어올 경우, json 조회
              axios.get('product.json')
-                .then(function(response){
-                    vm.productInfo = response.data[vm.index];
+                .then(function(response){                
+                        vm.name = response.data[vm.index].name; 
+                        vm.info = response.data[vm.index].info; 
+                        vm.price = response.data[vm.index].price; 
+                        vm.saleYn = response.data[vm.index].saleYn; 
+                        vm.disCountPrice = response.data[vm.index].disCountPrice; 
+                        vm.path = response.data[vm.index].path; 
                 })
                 .catch(function(error){
                     console.log(error);              
                 })
+             }
              
          },
     }
@@ -106,4 +158,16 @@ export default {
  div.container{
      max-width: none;
  }
+ img.productImg{
+    position: absolute; top:0; left: 0;
+    width: 100%;
+    height: 100%;
+ }
+ aside.infoClass{
+     padding-left: 5%;
+ }
+ select.form-control form-control-sm{
+     width:200px;margin-left:5px;
+ }
+    
 </style>
